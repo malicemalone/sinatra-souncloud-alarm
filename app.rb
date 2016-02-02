d818678707137c68e2e3f957d18b70d449c2a01b
Alice Malone <m.alice.malone@gmail.com>
2016-02-01 23:55:58 -0800

    adds styling

--- app.rb
+++ app.rb
@@ -1,15 +1,16 @@
 require 'sinatra'
 require 'dotenv'
+require 'soundcloud'
 Dotenv.load
 require_relative 'helper'
 set :root, File.dirname(__FILE__)
 TWILIO_SID = ENV["twilio_api_sid"]
 TWILIO_AUTH  = ENV["twilio_api_auth"]
+SC_CLIENT = ENV["souncloud_api_client"]
+SC_SECRET = ENV["soundcloud_api_secret"]
 
 
 get '/' do
-
-
 erb :index
 end
 
@@ -23,6 +24,10 @@ result = check_phone(@phone)
 
 if result == 0 && @time.length == 5
 	#api action
+	client = Soundcloud.new(:client_id => SC_CLIENT,
+                        :client_secret => SC_SECRET)
+
+	puts client.get('/me').username
 else
 	@message = "There was a problem with your request"
 end
--- helper.rb
+++ helper.rb
@@ -1,4 +1,3 @@
 def check_phone(number)
 	number =~ /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/
 end
-
